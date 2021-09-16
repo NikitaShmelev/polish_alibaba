9496f2585370ced645aa122ab7685d3ff7ef71f6
n.shmelyov <shmelev507@gmail.com>
2021-09-16 22:48:06 +0200

    test

---
+++ config/initializers/smtp.rb
@@ -0,0 +1,9 @@
+
+# ActionMailer::Base.smtp_settings = {
+#   domain: 'https://pprodukcja.herokuapp.com/',
+#   address:        "smtp.sendgrid.net",
+#   port:            587,
+#   authentication: :plain,
+#   user_name:      'polskaprodukcja03@gmail.com',
+#   api_key:       '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'
+# }
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # devise_for :users
+  # resources :users
+  resources :users, :only => [:index]
+  devise_for :users, controllers: {
+        sessions: 'users/sessions',
+        registrations: 'users/registrations',
+        confirmations: 'users/confirmations'
+      }
+  root to: 'home#index'
+end
