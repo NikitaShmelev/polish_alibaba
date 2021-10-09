46c3c09660ea2c38a1876b4c449b67fe446a3037
n.shmelyov <shmelev507@gmail.com>
2021-10-09 23:55:53 +0200

    db update

--- db/migrate/20211005014324_remove_fields_from_orders.rb
+++ db/migrate/20211005014324_remove_fields_from_orders.rb
@@ -1,7 +1,6 @@
 class RemoveFieldsFromOrders < ActiveRecord::Migration[6.1]
   def change
     remove_column :orders, :seller_name
-    remove_column :orders, :buyer_name
     rename_column :orders, :delivery_adress, :adress
     
   end
